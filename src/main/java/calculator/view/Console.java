312fa46bd82318977b133b0c38ce07109e5e1ccc
yeunsuh <yeunsuh.kim@gmail.com>
2025-02-07 22:29:40 +0900

    feat($main): created Console(MVC's view)
    Console's .read() & .print()
    : mapped UTC and java's method

---
+++ src/main/java/calculator/view/Console.java
@@ -0,0 +1,11 @@
+package calculator.view;
+
+public class Console {
+    public String read(){
+        return camp.nextstep.edu.missionutils.Console.readLine();
+    }
+
+    public void print(String arg){
+        System.out.println(arg);
+    }
+}
