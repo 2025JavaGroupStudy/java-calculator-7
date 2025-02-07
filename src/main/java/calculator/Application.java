417a24b471d91a64518612a2f521ca22a1cd5dd0
yeunsuh <yeunsuh.kim@gmail.com>
2025-02-07 22:34:30 +0900

    feat($main): completed Application
    completed, error catch for exiting

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,25 @@
 package calculator;
 
+import calculator.controller.CalculatorController;
+import calculator.view.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try{
+            new Application().run();
+
+        } catch (IllegalArgumentException e) {
+            System.err.println("ERROR : " + e.getMessage());
+            return;
+        }
+    }
+
+    public void run(){
+        Console console = new Console();
+        CalculatorController controller = new CalculatorController(console);
+
+        controller.input();
+        controller.process();
+        controller.output();
     }
 }
